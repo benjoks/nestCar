f2ef02c3e5b44afbb864506209ff8eb7debfb1dc
Benjamin Valdes Madriaza <benjoks@MacBook-Air-de-Benjamin.local>
2022-08-23 14:55:57 -0400

    finish commt

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { BRAND_SEED } from './data/brand.seed';
 import { CARS_SEED } from './data/cars.seed';
 import { CarsService } from '../cars/cars.service';
+import { BrandsService } from '../brands/brands.service';
 
 @Injectable()
 export class SeedService {
 
   constructor(
     private readonly carsService:CarsService,
+    private readonly BrandsService:BrandsService
   ){}
 
   populateDB(){
 
     this.carsService.fillCarsWithSeedData( CARS_SEED);
+    this.BrandsService.fillCarsWithSeedData( BRAND_SEED);
 
   }
 }
